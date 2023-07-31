70ea159488bb39954ef8c708393292ebb181c997
Brent Bottin <brentbottin@Panencos-MacBook-Pro.local>
2023-07-31 16:44:25 +0200

    clean up

--- src/create.migration.ts
+++
@@ -1,8 +0,0 @@
-import { MikroORM } from '@mikro-orm/core';
-import config from './orm.config';
-
-MikroORM.init(config).then(async (orm) => {
-  const migrator = orm.getMigrator();
-  const test = await migrator.createMigration();
-  await migrator.up();
-});
